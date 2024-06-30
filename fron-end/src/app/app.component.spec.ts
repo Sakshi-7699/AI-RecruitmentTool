094985e77089ae156750224d5b9bbdabb352680b
Sakshi Gupta <sakshigupta341999@gmail.com>
2024-06-30 14:47:24 +0100

    front-end: HttpClient added. Rollback to version 13.0.0

--- fron-end/src/app/app.component.spec.ts
+++ fron-end/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'webtool' title`, () => {
+  it(`should have the 'fron-end' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('webtool');
+    expect(app.title).toEqual('fron-end');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, webtool');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, fron-end');
   });
 });
