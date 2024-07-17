03f318b394a4b456b4551c668cbe8f25497022d4
Sakshi Gupta <sakshigupta341999@gmail.com>
2024-07-17 20:41:38 +0100

    front-end : form chips added for behavioral input

--- front-end/src/app/file-upload/file-upload.component.ts
+++ front-end/src/app/file-upload/file-upload.component.ts
@@ -4,6 +4,12 @@ import {FormBuilder, Validators, FormsModule, ReactiveFormsModule, FormGroup, Fo
 import { MatChipInputEvent } from '@angular/material/chips';
 import {COMMA, ENTER} from '@angular/cdk/keycodes';
 
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+import {ChangeDetectionStrategy,  computed, inject, model, signal} from '@angular/core';
+import {MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
+
+
+
 @Component({
   selector: 'app-file-upload',
   templateUrl: './file-upload.component.html',
@@ -33,6 +39,8 @@ export class FileUploadComponent {
     
     
   }
+
+  // File Upload  __________________________________________________________________________________
 
  
   @ViewChild('resumeInput', { static: false }) resumeInput!: ElementRef;
@@ -85,6 +93,52 @@ export class FileUploadComponent {
     }
   }
 
+
+  // behavioral module __________________________________________________________________________________
+
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  readonly currentFruit = model('');
+  readonly fruits = signal(['Leadership']);
+  readonly allFruits: string[] = ['Meritocracy', 'Leadership', 'Emotional Intelligence'];
+  readonly filteredFruits = computed(() => {
+    const currentFruit = this.currentFruit().toLowerCase();
+    return currentFruit
+      ? this.allFruits.filter(fruit => fruit.toLowerCase().includes(currentFruit))
+      : this.allFruits.slice();
+  });
+
+  readonly announcer = inject(LiveAnnouncer);
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our fruit
+    if (value) {
+      this.fruits.update(fruits => [...fruits, value]);
+    }
+
+    // Clear the input value
+    this.currentFruit.set('');
+  }
+
+  remove(fruit: string): void {
+    this.fruits.update(fruits => {
+      const index = fruits.indexOf(fruit);
+      if (index < 0) {
+        return fruits;
+      }
+
+      fruits.splice(index, 1);
+      this.announcer.announce(`Removed ${fruit}`);
+      return [...fruits];
+    });
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.fruits.update(fruits => [...fruits, event.option.viewValue]);
+    this.currentFruit.set('');
+    event.option.deselect();
+  }
 
   
 
--- front-end/src/app/home/home.component.ts
+++ front-end/src/app/home/home.component.ts
@@ -2,6 +2,17 @@ import {COMMA, ENTER} from '@angular/cdk/keycodes';
 import {Component} from '@angular/core';
 import {MatChipInputEvent} from '@angular/material/chips';
 
+
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+import {ChangeDetectionStrategy,  computed, inject, model, signal} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {MatAutocompleteModule, MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
+import { MatChipsModule} from '@angular/material/chips';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+
+
+
 export interface Fruit {
   name: string;
 }
@@ -10,8 +21,8 @@ export interface Fruit {
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
+  
 })
 export class HomeComponent {
-
   
 }
