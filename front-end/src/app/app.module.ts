84ab3fc31981e9474e130b56c534ff48b7011204
Sakshi Gupta <sakshigupta341999@gmail.com>
2024-07-19 03:53:25 +0100

    front-end : UI updates

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { AlgoResponseService } from './services/algo-response.service';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import {MatStepperModule} from '@angular/material/stepper';
 import {MatChipsModule} from '@angular/material/chips';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
@@ -50,6 +51,7 @@ import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
     MatAutocompleteModule,
     BrowserAnimationsModule,
     MatStepperModule,
+    MatProgressBarModule
   ],
   providers: [
     provideAnimationsAsync(),
